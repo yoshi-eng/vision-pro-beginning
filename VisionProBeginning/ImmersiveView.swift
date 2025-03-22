46b995cf70adbd8d3e1137455bf027304602be03
y <yoshidetl.eng@gmail.com>
2025-03-22 13:18:12 +0900

    通常のWindowに戻るエンティティを実装

--- VisionProBeginning/ImmersiveView.swift
+++ VisionProBeginning/ImmersiveView.swift
@@ -10,30 +10,36 @@ import RealityKit
 import RealityKitContent
 
 struct ImmersiveView: View {
-
+    @Environment(AppModel.self) private var appModel
+    @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
+    @Environment(\.openImmersiveSpace) private var openImmersiveSpace
+    @Environment(\.openWindow) private var openWindow
+    
     var body: some View {
         RealityView { content in
-            // Add the initial RealityKit content
-            if let immersiveContentEntity = try? await Entity(named: "Immersive", in: realityKitContentBundle) {
-                content.add(immersiveContentEntity)
-
-                // Put skybox here.  See example in World project available at
-                // https://developer.apple.com/
-            }
+            // 画面を戻るマテリアル
+            let model = ModelEntity(
+                mesh: .generateSphere(radius: 0.1),
+                materials: [SimpleMaterial(color: .white, isMetallic: true)])
             
-            // Check whether there's an attachment.
-//            if let attachment = attachments.entity(for: "MyAttachmentEntity") {
-//                
-//                attachment.name = viewAttachmentEntity.title.rawValue
-//                
-//                // Add it as a subentity of the plane.
-//                plane?.addChild(attachment)
-//                
-//                // Set the scale and position.
-//                attachment.scale = viewAttachmentEntity.scale
-//                attachment.setPosition(viewAttachmentEntity.position, relativeTo: yellowArch)
-//            }
+            // 自分の正面の1mの位置に配置
+            model.position = SIMD3<Float>(0.0, 0.0, -5.0)
+            
+            // Enable interactions on the entity.
+            model.components.set(InputTargetComponent())
+            model.components.set(CollisionComponent(shapes: [.generateSphere(radius: 0.1)]))
+            content.add(model)
         }
+        .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
+            Task { @MainActor in
+                // イマーシブを終了する
+                appModel.immersiveSpaceState = .inTransition
+                await dismissImmersiveSpace()
+                
+                // ウインドウを表示する
+                openWindow(id: appModel.windowGroupID)
+            }
+        })
     }
 }
 
