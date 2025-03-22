240ca1b8d65a334cd0d15fe7ceecd10be921ab0b
y <yoshidetl.eng@gmail.com>
2025-03-22 15:33:02 +0900

    DarkViewの動きを実装

--- VisionProBeginning/ImmersiveView.swift
+++ VisionProBeginning/ImmersiveView.swift
@@ -14,13 +14,15 @@ struct ImmersiveView: View {
     @Environment(\.dismissImmersiveSpace) private var dismissImmersiveSpace
     @Environment(\.openImmersiveSpace) private var openImmersiveSpace
     @Environment(\.openWindow) private var openWindow
-    @State var route = 9
+    @State var route = 1
     
     var body: some View {
         Group {
             switch route {
             case 0: LookBackView()
-            case 1: DarkView()
+            case 1: DarkView {
+                route = 9
+            }
             case 9:
                 // デバッグ用
                 RealityView { content in
